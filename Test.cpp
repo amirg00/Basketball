69cab69a4bb9fdfe43426f2f0e770d0075dcc6a0
AmirGill <amirg00@walla.com>
2022-05-26 14:03:10 +0300

    Add imports for testing

--- Test.cpp
+++ Test.cpp
@@ -1,1 +1,12 @@
 #include "doctest.h"
+#include "Team.hpp"
+#include "Schedule.hpp"
+using namespace b_ball;
+using namespace std;
+
+TEST_CASE(""){
+    League league{};
+    for (const &Team t : league.getTeams()){
+        cout << t.name() << endl;
+    }
+}
