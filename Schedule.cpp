c4db5cc4558e06404c61f560c94d5841a5f1c260
AmirGill <amirg00@walla.com>
2022-05-26 10:52:55 +0300

    Upload a draft of the Schedule class

--- Schedule.cpp
+++ Schedule.cpp
@@ -1,1 +1,5 @@
 #include "Schedule.hpp"
+
+Schedule::Schedule(League &league) {
+
+}
--- Schedule.hpp
+++ Schedule.hpp
@@ -1,15 +1,19 @@
 #pragma once
-
+#include "League.hpp"
+#include "Game.hpp"
 /*
-    BRIEF: in the season we have 2 cycles.
-           For each cycle, A is an indoor group and will play with any other group.
-           In second cycle, it would be the same, except that A will be an outdoor group
+    BRIEF: in the season we have 2 rounds.
+           For each round, A is an indoor group and will play with any other group.
+           In second round, it would be the same, except that A will be an outdoor group
            this time.
 
-    GOAL: Need to match the groups, such that in the end of the cycle,
+    GOAL: Need to match the groups, such that in the end of the round,
           each group had a game with every other group.
  */
 
 class Schedule {
-
+private:
+    vector<Game> _first_round, _second_round;
+public:
+    explicit Schedule(League& league);
 };
