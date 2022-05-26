b639f37cfce46bdac16399c585988facd7a3aada
AmirGill <amirg00@walla.com>
2022-05-26 14:28:48 +0300

    ADD a test runner

---
+++ TestCounter.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+using namespace doctest;
+
+int return_code = 0; // success
+
+struct ReporterCounter: public ConsoleReporter {
+    ReporterCounter(const ContextOptions& input_options)
+            : ConsoleReporter(input_options) {}
+};
+
+REGISTER_REPORTER("counter", 1, ReporterCounter);
+
+int main(int argc, char** argv) {
+    Context context;
+    context.addFilter("reporters", "counter");
+    context.run();
+    return return_code;
+}
