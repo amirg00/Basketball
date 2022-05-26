8456574d9c8acbe77ff55c6533a31bd3248c391d
AmirGill <amirg00@walla.com>
2022-05-26 17:15:09 +0300

    Add something to test

--- Test.cpp
+++ Test.cpp
@@ -1,14 +1,24 @@
 #include "doctest.h"
 #include "iostream"
-#include "Team.hpp"
-//#include "League.hpp"
-#include "Schedule.hpp"
+#include "sources/Team.hpp"
+#include "sources/League.hpp"
+#include "sources/Schedule.hpp"
 using namespace b_ball;
 using namespace std;
 
 TEST_CASE("Random League Constructor"){
+    cout << "***************** Teams Showcase *******************" << endl;
+    /*Create League*/
     League league{};
-    for (auto t : league.getTeams()){
+    for (const auto& t : league.getTeams()){
         cout << "Team Name: "  << t.name() << ", Team's skill: " << t.skill() << endl;
     }
+    cout << "****************************************************" << endl;
+
+    /*Create Schedule for games*/
+    Schedule schedule{league};
+
+
+    cout << "====================================================" << endl;
+
 }
