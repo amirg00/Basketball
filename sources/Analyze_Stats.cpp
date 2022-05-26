0c23c358a19ad258ada4ff1ebe704ddae9a8c7fc
AmirGill <amirg00@walla.com>
2022-05-26 19:16:49 +0300

    Add prototypes in comments

--- sources/Analyze_Stats.cpp
+++ sources/Analyze_Stats.cpp
@@ -1,1 +1,19 @@
 #include "Analyze_Stats.hpp"
+using namespace b_ball;
+
+Analyze_Stats::Analyze_Stats(const vector<vector<Game>>& first_cycle, const vector<vector<Game>>& second_cycle) {
+
+}
+//
+//size_t Analyze_Stats::longest_wins_row() const {
+//    return 0;
+//}
+//
+//size_t Analyze_Stats::longest_loses_row() const {
+//    return 0;
+//}
+//
+//vector<Team> Analyze_Stats::lead_teams() const {
+//    return vector<Team>();
+//}
+//
--- sources/Analyze_Stats.hpp
+++ sources/Analyze_Stats.hpp
@@ -1,5 +1,17 @@
 #pragma once
+#include "Schedule.hpp"
+#include "Game.hpp"
+#include "Team.hpp"
 
-class Analyze_Stats {
-
-};
+namespace b_ball{
+    class Analyze_Stats {
+    private:
+        vector<vector<Game>> _first_cycle, _second_cycle;
+    public:
+        Analyze_Stats(const vector<vector<Game>>& first_cycle, const vector<vector<Game>>& second_cycle);
+//        vector<Team>& lead_teams() const;
+//        size_t longest_wins_row() const;
+//        size_t longest_loses_row() const;
+//        size_t lead_points_teams const;
+    };
+}
