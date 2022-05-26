7a2e9e4ba14ca52e9bfec2db16505a3c3bbdb808
AmirGill <amirg00@walla.com>
2022-05-26 13:30:37 +0300

    Add scheduling for first & second cycles of tournament.

--- Schedule.cpp
+++ Schedule.cpp
@@ -1,5 +1,33 @@
 #include "Schedule.hpp"
+#include "iostream"
+using std::cout; using std::endl;
+
 
 Schedule::Schedule(League &league) {
+    size_t rounds = LEAGUE_TEAMS_AMOUNT/2;
+    vector<Team>& teams = league.getTeams();
 
+    for (size_t distance = 1; distance <= rounds; distance++){
+        cout << "Round "  << distance << endl;
+        vector<Game> curr_round_first_cycle;
+        vector<Game> curr_round_second_cycle;
+        for (size_t i = 0; i < rounds; i++) {
+            vector<Game> currRound;
+            size_t j = LEAGUE_TEAMS_AMOUNT - i - 1;
+            Game game_first_cycle{teams.at(i), teams.at(j)};
+            Game game_second_cycle{teams.at(j), teams.at(i)};
+            curr_round_first_cycle.push_back(game_first_cycle);
+            curr_round_second_cycle.push_back(game_second_cycle);
+            cout << "team " << teams[i].name() << " - team " << teams[LEAGUE_TEAMS_AMOUNT - i - 1].name() << endl;
+        }
+        _first_cycle.push_back(curr_round_first_cycle);
+        _second_cycle.push_back(curr_round_second_cycle);
+
+        Team& tmpTeam = teams.at(1);
+        for (size_t i = 1; i < LEAGUE_TEAMS_AMOUNT - 1; ++i) {
+              Team& next = teams.at(i+1);
+              teams.at(i+1) = tmpTeam;
+              tmpTeam = next;
+        }
+    }
 }
--- Schedule.hpp
+++ Schedule.hpp
@@ -2,18 +2,18 @@
 #include "League.hpp"
 #include "Game.hpp"
 /*
-    BRIEF: in the season we have 2 rounds.
-           For each round, A is an indoor group and will play with any other group.
-           In second round, it would be the same, except that A will be an outdoor group
+    BRIEF: in the season we have 2 cycles.
+           For each cycle, A is an indoor group and will play with any other group.
+           In second cycle, it would be the same, except that A will be an outdoor group
            this time.
 
-    GOAL: Need to match the groups, such that in the end of the round,
+    GOAL: Need to match the groups, such that in the end of the cycle,
           each group had a game with every other group.
  */
 
 class Schedule {
 private:
-    vector<Game> _first_round, _second_round;
+    vector<vector<Game>> _first_cycle, _second_cycle;
 public:
     explicit Schedule(League& league);
 };
