f266a16c26703455269c95ec7290682608beee8d
AmirGill <amirg00@walla.com>
2022-05-26 13:31:23 +0300

    Change constructor

--- Game.hpp
+++ Game.hpp
@@ -1,11 +1,10 @@
 #pragma once
 #include "Team.hpp"
-#include <utility>
 
 class Game {
 private:
     Team _indoor, _outdoor;
     size_t _indoor_points, _outdoor_points;
 public:
-    Game(Team indoor, Team outdoor);
+    Game(Team& indoor, Team& outdoor);
 };
