3b0bcd380fba1d898e68860616a92b822e8914e2
AmirGill <amirg00@walla.com>
2022-05-26 14:41:38 +0300

    Expand test

--- Test.cpp
+++ Test.cpp
@@ -1,12 +1,14 @@
 #include "doctest.h"
+#include "iostream"
 #include "Team.hpp"
+//#include "League.hpp"
 #include "Schedule.hpp"
 using namespace b_ball;
 using namespace std;
 
-TEST_CASE(""){
+TEST_CASE("Random League Constructor"){
     League league{};
-    for (const &Team t : league.getTeams()){
-        cout << t.name() << endl;
+    for (auto t : league.getTeams()){
+        cout << "Team Name: "  << t.name() << ", Team's skill: " << t.skill() << endl;
     }
 }
